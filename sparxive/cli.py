7a8889df48e3655112432fc4670cb9c0f85bf32e
Erik Hetzner <egh@e6h.org>
2013-11-23 09:46:10 -0800

    add archive command

--- sparxive/cli.py
+++ sparxive/cli.py
@@ -1,30 +1,43 @@
 import argparse
 import sys
 from sparxive.archive import Archive
+from sparxive.filer import Filer
+from os import path
 
 def main(rawargs=None):
      if rawargs is None: rawargs = sys.argv[1:]
      parser = argparse.ArgumentParser(description='sparkive - simple python archiver')
-     parser.add_argument('COMMAND', help='command to run', choices=['addversion', 'archive', 'extract', 'list', 'search'])
-     parser.add_argument('archive', help='archive file')
-     parser.add_argument('version_path', nargs="?", help='path to version to add', default=None)
+     subparsers = parser.add_subparsers()
+
+     archive = subparsers.add_parser('archive')
+     archive.add_argument('-r', '--root', help='root of archive', default='/tmp')
+     archive.add_argument('version_path', nargs="+", help='paths to version to add', default=None)
+     archive.set_defaults(command='archive')
+
+     addversion = subparsers.add_parser("addversion")
+     addversion.add_argument('archive', help='archive file')
+     addversion.add_argument('version_path', help='path to version to add', default=None)
+     addversion.set_defaults(command='addversion')
+
+     ls = subparsers.add_parser("list")
+     ls.add_argument('archive', help='archive file')
+     ls.set_defaults(command='list')
+
      args = parser.parse_args(rawargs)
-     if args.COMMAND == "addversion":
-          if (args.version_path is None):
-               sys.stderr("You must provide a path to a version to add!")
-          else:
-               a = Archive(args.archive)
-               a.add_version(args.version_path)
-     elif args.COMMAND == "extract":
-          pass
-     elif args.COMMAND == "list":
+     if args.command == "addversion":
+          a = Archive(args.archive)
+          a.add_version(args.version_path)
+     elif args.command == "archive":
+          filer = Filer(path.abspath(args.root))
+          for p in args.version_path:
+               archive = filer.find_archive(p)
+               archive.add_version(p)
+     elif args.command == "list":
           d = Archive(args.archive).list()
           for n in sorted(d.keys()):
                print "version %d:"%n
                for (p, dt) in d[n]:
                     print "  %s  (%s)"%(p, dt.strftime("%Y-%m-%d %H:%M:%S"))
-     elif args.COMMAND == "search":
-          pass
 
 if __name__ == "__main__":
     main()
--- tests/cli_test.py
+++ tests/cli_test.py
@@ -3,8 +3,10 @@ from nose.tools import assert_equal
 import sparxive.cli
 from sparxive import mkstemppath
 import os
+from os import path
 from sparxive import rzip
 from zipfile import ZipFile
+from tempfile import mkdtemp
 
 class CliTest(TestCase):
     def setUp(self):
@@ -27,3 +29,9 @@ class CliTest(TestCase):
                     filenames.append(info.filename)
                 assert_equal(filenames, ["0/foobar/foo", "1/foobar/bar"])
         sparxive.cli.main(["list", a])
+        
+    def test_cli_archive(self):
+        archive = mkdtemp()
+        foo = path.join('foobar', 'foo')
+        sparxive.cli.main(['archive', '-r', archive, foo])
+        assert(path.exists(path.join(archive, '2013', '11', 'foo.zip.rz')))
