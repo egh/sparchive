b7390a9d242e24e9b8b0b80fd672f50c503fd3cd
Erik Hetzner <egh@e6h.org>
2013-11-23 09:01:03 -0800

    test that filer.find_file returns None when it finds nothing

--- tests/filer_test.py
+++ tests/filer_test.py
@@ -8,8 +8,8 @@ import os
 class FilerTest(TestCase):
     def test_find_file(self):
         filer = Filer('.')
-        file = filer.find_file('foo')
-        assert_equal(os.path.abspath(os.path.join('tests', 'fixtures', 'foobar', 'foo')), file)
+        assert_equal(os.path.abspath(os.path.join('tests', 'fixtures', 'foobar', 'foo')), filer.find_file('foo'))
+        assert_equal(None, filer.find_file("NONE"))
 
     def test_get_mtime(self):
         filer = Filer('.')
