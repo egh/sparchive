2eb761fcc7adb822ae3a440f64118eac5011b995
Erik Hetzner <egh@e6h.org>
2013-11-23 00:35:51 -0800

    only require version arg for add

--- sparxive/cli.py
+++ sparxive/cli.py
@@ -6,12 +6,15 @@ def main(rawargs=None):
      if rawargs is None: rawargs = sys.argv[1:]
      parser = argparse.ArgumentParser(description='sparkive - simple python archiver')
      parser.add_argument('COMMAND', help='command to run', choices=['add', 'extract', 'list', 'search'])
-     parser.add_argument('archive', nargs=1, help='archive file')
-     parser.add_argument('version_path', nargs=1, help='path to version to add')
+     parser.add_argument('archive', help='archive file')
+     parser.add_argument('version_path', nargs="?", help='path to version to add', default=None)
      args = parser.parse_args(rawargs)
      if (args.COMMAND == "add"):
-         a = Archive(args.archive[0])
-         a.add_version(args.version_path[0])
+          if (args.version_path is None):
+               sys.stderr("You must provide a path to a version to add!")
+          else:
+               a = Archive(args.archive)
+               a.add_version(args.version_path)
      elif args.COMMAND == "extract":
           pass
      elif args.COMMAND == "list":
