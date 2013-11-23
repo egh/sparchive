70df0ec53d0ae5483f3526b8a26c9ea6c1b5b0b0
Erik Hetzner <egh@e6h.org>
2013-11-23 00:26:47 -0800

    remove nargs for command

--- sparxive/cli.py
+++ sparxive/cli.py
@@ -5,22 +5,22 @@ from sparxive.archive import Archive
 def main(rawargs=None):
      if rawargs is None: rawargs = sys.argv[1:]
      parser = argparse.ArgumentParser(description='sparkive - simple python archiver')
-     parser.add_argument('COMMAND', nargs=1, help='command to run', choices=['add', 'extract', 'list', 'search'])
+     parser.add_argument('COMMAND', help='command to run', choices=['add', 'extract', 'list', 'search'])
      parser.add_argument('archive', nargs=1, help='archive file')
      parser.add_argument('version_path', nargs=1, help='path to version to add')
      args = parser.parse_args(rawargs)
-     if (args.COMMAND[0] == "add"):
+     if (args.COMMAND == "add"):
          a = Archive(args.archive[0])
          a.add_version(args.version_path[0])
-     elif args.COMMAND[0] == "extract":
+     elif args.COMMAND == "extract":
           pass
-     elif args.COMMAND[0] == "list":
+     elif args.COMMAND == "list":
           d = Archive(args.archive[0]).list()
           for n in sorted(d.keys()):
                print "version %d:"%n
                for (p, dt) in d[n]:
                     print "  %s  (%s)"%(p, dt.strftime("%Y-%m-%d %H:%M:%S"))
-     elif args.COMMAND[0] == "search":
+     elif args.COMMAND == "search":
           pass
 
 if __name__ == "__main__":
